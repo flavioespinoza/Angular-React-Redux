dae1f16b5ff75bec239366a8f80606ff59927d27
flavioespinoza <flavio.espinoza@gmail.com>
2018-06-06 07:44:50 -0600

    Added my React Stockcharts lib.

--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,7 @@
 export function uniq_all_props(__arr1, __arr2) {
-  let arr = __arr1.concat(__arr2)
-  let set = []
-  let result = []
+  const arr = __arr1.concat(__arr2)
+  const set = []
+  const result = []
   arr.forEach(function (__obj) {
     set.push([__obj].map(function (val, key) {
       return JSON.stringify(key) + '_' + JSON.stringify(val)
